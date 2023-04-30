6fa2523d865df18d7b58b7a57b49d04d57378145
l3vth <132221005+l3vth@users.noreply.github.com>
2023-04-30 22:35:59 +0200

    Add files via upload

---
+++ pasta calculator.py
@@ -0,0 +1,23 @@
+print("")
+portions = int(input("How many portions do you want to make? " ))
+eggs = portions * 2
+flour = eggs * 100 
+print("")
+
+for x in ["you need " + str(eggs) + " eggs and "+ str(flour) + "g of flour ",
+          "",
+          "mix in a bow until its all combined. Then you are going to kneed it for about 15 minutes until its a hard dough and let it rest for about one hour.",
+           "",
+          "After one hour you cut the dough into smaller pieces and roll it out very thin or how thick you want your pasta. ",
+          "",
+          "now just cut out the pasta shape that you like, boil it in salted water for only 3-4 minutes and you are done!",
+          "",
+          "enjoy your pasta with a creamy sauce and Bon Appétit."]:
+    print(x)
+
+print("")
+print("")
+print("")
+print("")
+print("")
+input("press any button to quit")
